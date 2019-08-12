3a3c42714827903d755af0edff19aed456e5f7b3
vinoth-kk <53612192+vinoth-kk@users.noreply.github.com>
2019-08-12 18:48:41 +1000

    Updated the Test Case to cover all the scenarios

--- src/test/java/com/me/trans/AccountTransactionTest.java
+++ src/test/java/com/me/trans/AccountTransactionTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.me.trans.constants.Constant;
@@ -24,34 +23,44 @@ public class AccountTransactionTest {
 		File csvFile = new File(Constant.CSV_PATH);
 		List<Transaction> collectedTransaction = null;
 		collectedTransaction = TransactionUtility.fetchTransactionsFromCSVFile(csvFile);
-		assertEquals(collectedTransaction.get(0).getTransactionId(), "TX10001");
+		assertEquals(collectedTransaction.get(0).getTransactionId(), "TX10001", "CSV Read successful");
 	}
 
 	@Test
 	public void testCSVReadWithWrongPath() throws IOException {
 		File csvFile = new File("resources/sample_data1.csv");
-		assertThrows(FileNotFoundException.class, () -> TransactionUtility.fetchTransactionsFromCSVFile(csvFile));
+		assertThrows(FileNotFoundException.class, 
+				() -> TransactionUtility.fetchTransactionsFromCSVFile(csvFile),
+											"CSV Read failure due to wrong path");
 	}
 
 	@Test
 	public void testForwrongFromDate() throws IOException {
 		assertThrows(DateTimeParseException.class, () -> AccountTransaction.fetchRelativeBalanceDetailsForTransaction("ACC334455", "20-10-2018 12:00:00",
-				"20/10/2018 19:00:00"));
+				"20/10/2018 19:00:00"), "From Date given in wrong format");
 		
 	}
 
 	@Test
 	public void testForwrongToDate() throws IOException {
 		assertThrows(DateTimeParseException.class, () -> AccountTransaction.fetchRelativeBalanceDetailsForTransaction("ACC334455", "20/10/2018 12:00:00",
-				"20-10-2018 19:00:00"));
+				"20-10-2018 19:00:00"), "To Date given in wrong format");
 		
 	}
 
 	@Test
-	public void testForCorrectData() throws IOException {
+	public void testForCorrectDataWithReversal() throws IOException {
 		RelativeAccountBalance relativeAccountBalance = AccountTransaction
 				.fetchRelativeBalanceDetailsForTransaction("ACC334455", "20/10/2018 12:00:00", "20/10/2018 19:00:00");
 		assertEquals(relativeAccountBalance.getRelativeBalance().toString(), "-30.50");
+		assertEquals(relativeAccountBalance.getTransactionCount(), 3);
+	}
+	
+	@Test
+	public void testForCorrectDataWithoutReversal() throws IOException {
+		RelativeAccountBalance relativeAccountBalance = AccountTransaction
+				.fetchRelativeBalanceDetailsForTransaction("ACC998877", "20/10/2018 12:00:00", "20/10/2018 19:00:00");
+		assertEquals(relativeAccountBalance.getRelativeBalance().toString(), "0.50");
 		assertEquals(relativeAccountBalance.getTransactionCount(), 3);
 	}
 
